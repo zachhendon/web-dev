3bebe087968bbcb859fa46ea5f915c5dfcba47e6
Zach Hendon <zachendo3@gmail.com>
2023-05-19 23:04:37 -0500

    working crawler

--- 11-web_crawler/crawl.js
+++ 11-web_crawler/crawl.js
@@ -1,7 +1,20 @@
 const { JSDOM } = require("jsdom");
 const axios = require("axios");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+  
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+  pages[normalizedCurrentURL] = 1;
+  
   console.log(`actively crawling: ${currentURL}`);
 
   try {
@@ -10,7 +23,7 @@ async function crawlPage(currentURL) {
       console.log(
         `error in fetch with status code: ${response.status} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = response.headers["content-type"];
@@ -18,13 +31,19 @@ async function crawlPage(currentURL) {
       console.log(
         `non html response, content type: ${contentType} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
-    console.log(response.data);
+    const htmlBody = response.data;
+
+    nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(`error in fetch: ${err.message} on page: ${currentURL}`);
   }
+  return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
