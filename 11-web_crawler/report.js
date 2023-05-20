a1a6b549900b926597b07279b10b116e5d16b92c
Zach Hendon <zachendo3@gmail.com>
2023-05-20 16:34:27 -0500

    link for 1 hit and links for 1+ hits

--- 11-web_crawler/report.js
+++ 11-web_crawler/report.js
@@ -6,7 +6,7 @@ function printReport(pages) {
   for (const sortedPage of sortedPages) {
     const url = sortedPage[0];
     const hits = sortedPage[1];
-    console.log(`Found ${hits} links to page: ${url}`);
+    console.log(`Found ${hits} link${hits === 1 ? "" : "s"} to page: ${url}`);
   }
   console.log("================================================");
   console.log("END REPORT");
