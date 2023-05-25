e579022ad57b8246d38c9941d39e3549c37f56c6
Zach Hendon <zachendo3@gmail.com>
2023-05-25 13:48:04 -0500

    search for posts

--- 10-reddit_clone/src/features/posts/postsSlice.js
+++ 10-reddit_clone/src/features/posts/postsSlice.js
@@ -1,28 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  posts: {},
+  posts: [],
   sortedPosts: {},
+  status: "idele",
 };
+
+export const searchPosts = createAsyncThunk(
+  "posts/searchPosts",
+  async (query) => {
+    const response = await axios.get("https://www.reddit.com/search.json", {
+      params: { q: query, limit: "10"},
+    });
+    return response.data.data.children.map((post) => post.data.title);
+  }
+);
 
 const postsSlice = createSlice({
   name: "posts",
   initialState: initialState,
   reducers: {
     addPost: (state, action) => {},
-    searchPosts: (state, action) => {},
     filterPosts: (state, action) => {},
     collapseComment: (state, action) => {},
     expandComment: (state, action) => {},
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(searchPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(searchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(searchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
 
-export const {
-  addPost,
-  searchPosts,
-  filterPosts,
-  collapseComment,
-  expandComment,
-} = postsSlice.actions;
+export const { addPost, filterPosts, collapseComment, expandComment } =
+  postsSlice.actions;
 
 export default postsSlice.reducer;
--- 10-reddit_clone/src/pages/Homepage/Homepage.jsx
+++ 10-reddit_clone/src/pages/Homepage/Homepage.jsx
@@ -1,29 +1,61 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { searchPosts } from "../../features/posts/postsSlice";
+import { useState, useEffect } from "react";
 
 export default function Homepage() {
+  const [value, setValue] = useState("");
+  const [displayPosts, setDisplayPosts] = useState("");
+  const posts = useSelector((state) => state.posts.posts);
+  const status = useSelector((state) => state.posts.status);
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (value !== "") {
+      dispatch(searchPosts(value));
+    }
+  };
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    switch (status) {
+      case "loading":
+        setDisplayPosts(<p>Loading...</p>);
+        break;
+      case "succeeded":
+        setDisplayPosts(posts.map((post, index) => <p key={index}>{"- " + post}</p>));
+        break;
+      case "failed":
+        setDisplayPosts(<p>Failed to load posts</p>);
+        break;
+      default:
+        setDisplayPosts(<p>Search for posts</p>);
+        break;
+    }
+  }, [status, posts]);
+
   return (
     <>
       <Link to="/post/1" style={{ textDecoration: "none" }}>
         <h1 className={styles.test}>Homepage</h1>
       </Link>
+
       <main>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit illo
-          accusantium error dolores veniam tempore, fugiat ut dignissimos ea qui
-          eius inventore excepturi expedita optio quasi? Iste enim consequatur
-          voluptates architecto. Reprehenderit molestias perspiciatis, ad ex
-          tempore quasi recusandae consequuntur ullam. Nihil, ipsa quae rerum
-          provident eveniet quibusdam expedita voluptas commodi tempore
-          blanditiis. Blanditiis iusto dicta quas quae ipsa nobis pariatur
-          quibusdam nostrum tenetur modi repudiandae iure eius molestias
-          repellat at hic, distinctio molestiae, corrupti suscipit laudantium
-          veniam provident vitae? Sequi distinctio quam eaque sed vero
-          provident, possimus quis culpa explicabo repellat rerum neque
-          similique iste dicta sapiente. Ea dicta ipsam, modi sed at voluptatum
-          quam tempora similique ipsa, officiis labore recusandae ab fuga vel
-          cumque incidunt quia. Dolor, cum?
-        </p>
+        <button onClick={handleClick}>Search</button>
+        <form onSubmit={handleClick}>
+          <input
+            type="text"
+            value={value}
+            placeholder="search"
+            onInput={handleInput}
+          />
+        </form>
+        {displayPosts}
       </main>
     </>
   );
