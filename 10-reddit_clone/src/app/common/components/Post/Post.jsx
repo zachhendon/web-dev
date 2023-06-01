42cdbbf2feebc37ad1547685c0560ac4895a9d2a
Zach Hendon <zachendo3@gmail.com>
2023-06-01 14:47:26 -0500

    upvote and downvote

--- 10-reddit_clone/src/app/common/components/Post/Post.jsx
+++ 10-reddit_clone/src/app/common/components/Post/Post.jsx
@@ -3,9 +3,18 @@ import styles from "./Post.module.css";
 import PostUps from "../PostUps/PostUps";
 
 function Post(props) {
+  const error = (
+    <>
+      <p>Sorry, there was an error loading this post</p>
+      <p>
+        ---------------------------------------------------------------------------------------------------------------------------------
+      </p>
+    </>
+  );
+
   const posts = useSelector((state) => state.posts.posts);
   if (posts.length <= props.postIndex) {
-    return;
+    return error;
   }
   const post = posts[props.postIndex];
   const title = post.data.title;
@@ -17,6 +26,9 @@ function Post(props) {
   };
   let media;
   if (post.data.is_video) {
+    if (!post.data.media) {
+      return error;
+    }
     // video
     media = (
       <video
@@ -38,7 +50,7 @@ function Post(props) {
     } else if (post.data.thumbnail && isValidMedia(post.data.thumbnail)) {
       imgUrl = post.data.thumbnail;
     } else {
-      return;
+      return error;
     }
     media = (
       <img
@@ -54,7 +66,7 @@ function Post(props) {
     );
   }
   const subreddit = post.data.subreddit;
-  const ups = <PostUps ups={post.data.ups} />;
+  const ups = <PostUps ups={post.data.ups} postIndex={props.postIndex} />;
 
   return (
     <>
--- 10-reddit_clone/src/app/common/components/PostUps/PostUps.jsx
+++ 10-reddit_clone/src/app/common/components/PostUps/PostUps.jsx
@@ -1,15 +1,53 @@
+import { useState } from "react";
 import styles from "./PostUps.module.css";
 import ArrowUp from "../../assets/ArrowUp";
 import ArrowDown from "../../assets/ArrowDown";
+import React from "react";
 
 function PostUps(props) {
-  const ups = props.ups;
+  const [ups, setUps] = useState(props.ups);
+
+  const handleClick = (direction) => {
+    const upsDiv = document.getElementById(styles.ups + props.postIndex);
+    const upSvgPath = upsDiv.children[0].children[0].children[0];
+    const downSvgPath = upsDiv.children[2].children[0].children[0];
+    const upvoteStatus = ups - props.ups;
+
+    if (direction === 1) {
+      upSvgPath.style.stroke = "#ff4500";
+    } else {
+      downSvgPath.style.stroke = "#00b38f";
+    }
+
+    if (ups === props.ups) {
+      setUps(ups + direction);
+    } else if (upvoteStatus === direction) {
+      upSvgPath.style.stroke = "#373231";
+      downSvgPath.style.stroke = "#373231";
+      setUps(props.ups);
+    } else if (upvoteStatus === -direction) {
+      setUps(ups + 2 * direction);
+      if (direction === 1) {
+        downSvgPath.style.stroke = "#373231";
+      } else {
+        upSvgPath.style.stroke = "#373231";
+      }
+    }
+  };
 
   return (
-    <div className={"flex " + styles.ups}>
-      {ArrowUp}
-      <h4>{ups}</h4>
-      {ArrowDown}
+    <div
+      style={{ color: "red" }}
+      id={styles.ups + props.postIndex}
+      className={"flex " + styles.ups}
+    >
+      <div className={styles.up} onClick={() => handleClick(1)}>
+        {ArrowUp}
+      </div>
+      <h4 style={{ userSelect: "none" }}>{ups}</h4>
+      <div className={styles.down} onClick={() => handleClick(-1)}>
+        {ArrowDown}
+      </div>
     </div>
   );
 }
